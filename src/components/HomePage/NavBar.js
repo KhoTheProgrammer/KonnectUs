d087c6bae4956d02dcdde37189413d475d99b60f
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-08 01:22:25 +0200

    Created  space between the logo and the tab links by pushing the latter to the end

--- src/components/HomePage/NavBar.js
+++ src/components/HomePage/NavBar.js
@@ -2,20 +2,26 @@ import logo from "./logo.jpg";
 
 export default function NavBar() {
   return (
-    <div className="grid grid-cols-[1fr_3fr] p-5">
+    <div className="grid grid-cols-[1fr_3fr] p-3">
       <p className="font-bold text-5xl"> KonnectUs</p>
-      <ul className="flex mx-4 p-3">
-      <li className=" font-bold mx-6 text-lg hover:text-green-500">Home</li>
-        <li className=" font-bold mx-6 text-lg hover:text-green-500">About Us</li>
-        <li className=" font-bold mx-6 text-lg hover:text-green-500">Products</li>
+      <ul className="flex p-3 justify-end">
+        <li className=" font-bold mx-6 text-lg hover:text-green-500">Home</li>
+        <li className=" font-bold mx-6 text-lg hover:text-green-500">
+          About Us
+        </li>
+        <li className=" font-bold mx-6 text-lg hover:text-green-500">
+          Products
+        </li>
         <li className=" font-bold mx-6 text-lg hover:text-green-500">Team</li>
-        <li className=" font-bold mx-6 text-lg hover:text-green-500">Contact Us</li>
+        <li className=" font-bold mx-6 text-lg hover:text-green-500">
+          Contact Us
+        </li>
         <li className=" font-bold mx-6 text-lg hover:text-green-500">Login</li>
       </ul>
       <input
         type="search"
         placeholder="search"
-        className=" h-10 mt-2 ml-[90px] border-[1px] border-black text-center rounded-xl mx-6 "
+        className=" border-[1px] border-black rounded-xl mx-6 text-center p-[2px]"
       ></input>
     </div>
   );
