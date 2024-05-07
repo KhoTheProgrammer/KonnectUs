4db49d742e911838e0cf488c8953d952223180f2
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-08 01:15:34 +0200

    Increased the space between the navbar tab links and added a hover effect on them

--- src/components/HomePage/NavBar.js
+++ src/components/HomePage/NavBar.js
@@ -5,12 +5,12 @@ export default function NavBar() {
     <div className="grid grid-cols-[1fr_3fr] p-5">
       <p className="font-bold text-5xl"> KonnectUs</p>
       <ul className="flex mx-4 p-3">
-        <li className=" mx-5 font-bold">Home</li>
-        <li className=" mx-5 font-bold">About Us</li>
-        <li className=" mx-5 font-bold">Products</li>
-        <li className=" mx-5 font-bold">Team</li>
-        <li className=" mx-5 font-bold">Contact Us</li>
-        <li className=" mx-5 font-bold">Login</li>
+      <li className=" font-bold mx-6 text-lg hover:text-green-500">Home</li>
+        <li className=" font-bold mx-6 text-lg hover:text-green-500">About Us</li>
+        <li className=" font-bold mx-6 text-lg hover:text-green-500">Products</li>
+        <li className=" font-bold mx-6 text-lg hover:text-green-500">Team</li>
+        <li className=" font-bold mx-6 text-lg hover:text-green-500">Contact Us</li>
+        <li className=" font-bold mx-6 text-lg hover:text-green-500">Login</li>
       </ul>
       <input
         type="search"
