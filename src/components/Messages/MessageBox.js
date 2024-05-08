7739f199dc157e7bf42004870e85803154ae1bf4
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-05-08 17:19:47 +0200

    updated the styling for the message box

--- src/components/Messages/MessageBox.js
+++ src/components/Messages/MessageBox.js
@@ -1,26 +1,28 @@
-//this file describes the component(text box) which users type into when sending a message 
+//this file describes the component(text box) which users type into when sending a message
 
 export default function MessageBox() {
   const handleSubmit = () => {
-      //to be defined later
-    };
+    //to be defined later
+  };
 
   return (
     <div>
-
-
-      <ul className="flex flex-row list-none">
-        <li>
+      <form onSubmit={handleSubmit}>
+        <div className="flex items-center px-3 py-2 rounded-lg">
           <textarea
             name="Message"
-            placeholder="Messages"
-            onSubmit={handleSubmit}
+            placeholder="Message"
+            row="1"
+            className="block mx-4 p-2.5 w-full text-sm text-black bg-white rounded-lg border border-green-500 focus:ring-green-900 focus:border-green-900 placeholder-slate-300"
           ></textarea>
-        </li>
-        <li>
-          <button type="submit">send</button>
-        </li>
-      </ul>
+          <button
+            type="submit"
+            className="inline-flex justify-center p-2 text-white bg-green-700 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            send
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,10 +1,12 @@
 //This file creates the messaging page when users communicate with each other
 
+import UserMessages from "./UserMessages";
 import MessageBox from "./MessageBox";
 
 export default function Messages() {
   return (
     <div>
+      <UserMessages/>  
       <MessageBox />
     </div>
   );
