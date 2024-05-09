9e7ace77e13af7283ca70f59eb87a92f4499e83b
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-05-09 11:41:16 +0200

    Added the UserMessages component that creates a formatted list of message tiles on the UI

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -4,10 +4,35 @@ import UserMessages from "./UserMessages";
 import MessageBox from "./MessageBox";
 
 export default function Messages() {
+
+  //sample data that is used to test the Messaging components
+  let data = [
+    {
+      text: "Hey, James. It's Johnson",
+      id: "me",
+      timestamp: "1",
+    },
+    {
+      text: "Oh, Hey Johnson. Wassup?",
+      id: "other",
+      timestamp: "2",
+    },
+    {
+      text: "Nothing much. I'm just chilling",
+      id: "me",
+      timestamp: "3",
+    },
+  ];
   return (
-    <div>
-      <UserMessages/>  
-      <MessageBox />
+    <div className=" flex flex-col">
+      <ul>
+        <li>
+          <UserMessages messages={data} />
+        </li>
+        <li>
+          <MessageBox />
+        </li>
+      </ul>
     </div>
   );
 }
