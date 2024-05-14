80521a9c6b927d192213e9c0adcea3ffd7c2c153
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-14 23:28:18 +0200

    Changed text color to white, added some padding to the div and added a border-rdius to the input fields

--- src/components/AccountManagement/Change.js
+++ src/components/AccountManagement/Change.js
@@ -1,36 +1,33 @@
-export default function Change() {
+export default function Change({password, setPassword}) {
+
   return (
-    <div className=" bg-[#392a05] p-4">
-      {" "}
-      <h2 className="text-center font-medium text-xl">CHANGE PASSWORD</h2>
+    <div className=" bg-green-300 w-full px-24 py-16 rounded-2xl mt-16">
+      <h2 className="text-center font-bold text-3xl text-white mb-8">CHANGE PASSWORD</h2>
       <form>
-        <label className=" text-lg">Old Password</label>
-        <br></br>
+        <label className=" text-lg font-medium text-white">Old Password</label> <br></br>
         <input
-          type="password"
+          type="text"
           placeholder="old password"
           required
-          className="p-1 w-full text-black"
-        ></input>
+          className="py-4 w-full text-white mb-8 px-2 rounded-lg"
+        ></input>{" "}
         <br></br>
-        <label className=" text-lg">New Password</label>
-        <br></br>
+        <label className=" text-lg font-medium text-white">New Password</label> <br></br>
         <input
-          type="password"
-          placeholder="new password"
+          type="email"
+          placeholder="new password..."
           required
-          className="p-1 w-full text-black"
-        ></input>
+          className="py-4 px-2 w-full  text-white mb-8 rounded-lg"
+        ></input>{" "}
         <br></br>
-        <label className=" text-lg">Confirm Password</label>
-        <br></br>
+        <label className=" text-lg font-medium text-white">Confirm Password</label> <br></br>
         <input
-          type="password"
+          type="tel"
           placeholder="confirm password"
           required
-          className="p-1 w-full text-black"
+          className="py-4 w-full text-black px-2 mb-8 rounded-lg"
         ></input>
-        <button className="block m-auto bg-white mt-3 p-1 text-black font-bold">
+        <button className="block m-auto bg-white mt-3 p-2 text-black font-bold rounded-lg">
           CHANGE
         </button>
       </form>
