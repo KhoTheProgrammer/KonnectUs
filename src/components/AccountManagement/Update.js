61425f422dde86bdb0c7717a032b789dd08ebbda
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-14 23:26:50 +0200

    Changed text color to white and added some padding to the div

--- src/components/AccountManagement/Update.js
+++ src/components/AccountManagement/Update.js
@@ -1,32 +1,32 @@
 export default function Update() {
   return (
-    <div className=" bg-[#392a05] p-4">
-      <h2 className="text-center font-medium text-xl">UPDATE INFORMATION</h2>
+    <div className=" bg-green-300 w-full px-24 py-16 rounded-2xl">
+      <h2 className="text-center font-bold text-3xl text-white mb-8">UPDATE INFORMATION</h2>
       <form>
-        <label className=" text-lg">Name</label> <br></br>
+        <label className=" text-lg font-medium text-white">Name</label> <br></br>
         <input
           type="text"
           placeholder="your name..."
           required
-          className="p-1 w-full text-black"
+          className="py-4 w-full text-white mb-8 px-2 rounded-lg"
         ></input>{" "}
         <br></br>
-        <label className=" text-lg">Email</label> <br></br>
+        <label className=" text-lg font-medium text-white">Email</label> <br></br>
         <input
           type="email"
           placeholder="your email..."
           required
-          className="p-1 w-full text-black"
+          className="py-4 px-2 w-full  text-white mb-8 rounded-lg"
         ></input>{" "}
         <br></br>
-        <label className=" text-lg">Phone Number</label> <br></br>
+        <label className=" text-lg font-medium text-white">Phone Number</label> <br></br>
         <input
           type="tel"
           placeholder="your phone number..."
           required
-          className="p-1 w-full text-black"
+          className="py-4 w-full text-black px-2 mb-8 rounded-lg"
         ></input>
-        <button className="block m-auto bg-white mt-3 p-1 text-black font-bold">
+        <button className="block m-auto bg-white mt-3 p-2 text-black font-bold rounded-lg">
           UPDATE
         </button>
       </form>
