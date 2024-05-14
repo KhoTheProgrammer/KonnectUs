78b91fcad33a5d3499c2145337d4edb812f3c313
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-14 23:38:10 +0200

    Changed text color to white from black

--- src/components/AccountManagement/SideBar.js
+++ src/components/AccountManagement/SideBar.js
@@ -3,10 +3,10 @@ export default function SideBar() {
     <div>
       <div className=" h-screen bg-green-300 w-52">
         <div className=" p-4">
-          <p className=" text-black font-bold mb-7 text-center text-xl">DASHBOARD</p>
-          <p className=" text-black font-bold border-b-2 mb-4">USERS</p>
-          <p className=" text-black font-bold  mb-4">REPORTS</p>
-          <p className=" text-black font-bold  mb-4">LOGOUT</p>
+          <p className=" text-white font-bold mb-7 text-center text-xl">DASHBOARD</p>
+          <p className=" text-white font-bold border-b-2 mb-4">USERS</p>
+          <p className=" text-white font-bold  mb-4">REPORTS</p>
+          <p className=" text-white font-bold  mb-4">LOGOUT</p>
           
         </div>
       </div>
