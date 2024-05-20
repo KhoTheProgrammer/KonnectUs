a2cddf782af2644db166b356aaa768d0037b99ac
Rbitah <bsc-35-21@unima.ac.mw>
2024-05-20 19:18:16 +0200

    staging to solving conflicts

--- src/routes.js
+++ src/routes.js
@@ -1,42 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
-import ReviewRating from "./components/RatingAndReviews/rateAndReview";
-import Messages from "./components/Messages/Messages";
-import Login from "./components/Login/Login";
 import HomePage from "./components/HomePage/HomePage";
-import ProductsPage from "./components/Alt/ProductsPage";
-import Team from "./components/Team/Team";
-
-// Variable to store routes
-const router = createBrowserRouter([
+import HomeAfterLogin from "src/components/HomeAfterLogin/HomePageLogin.js"
+const router = createBrowserRouter(
+    [
   {
-    path: "/",
-    element: <App></App>,
+            path:"/",
+            element:<HomePage></HomePage>
   },
   {
-    path: "/rateAndReview",
-    element: <ReviewRating />,
-  },
-  {
-    path: "/Messages",
-    element: <Messages></Messages>,
-  },
-  {
-    path: "/Login",
-    element: <Login></Login>,
-  },
-  {
-    path: "/HomePage",
-    element: <HomePage></HomePage>
-  },
-  {
-    path: "ProductsPage",
-    element: <ProductsPage></ProductsPage>
-  },
-  {
-    path:"/Team",
-    element: <Team></Team>
-  }
-]);
-
-export default router;
+            path:"src\components\HomeAfterLogin\HomePageLogin",
+            element:<HomeAfterLogin></HomeAfterLogin>
+        }
+    ]
+)
