fde3d6629b38c14f7027862715d526ae7c0e78f6
“osmanDaud” <imwakabira@unima.ac.mw>
2024-05-20 19:28:02 +0200

    routes changes

--- src/routes.js
+++ src/routes.js
@@ -1,38 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
-import ProductsPage from "./components/Alt/ProductsPage";
-import Team from "./components/Team/Team";
-
-// Variable to store routes
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>,
-  },
-  {
-    path: "/rateAndReview",
-    element: <ReviewRating />,
-  },
-  {
-    path: "/Messages",
-    element: <Messages></Messages>,
-  },
-  {
-    path: "/Login",
-    element: <Login></Login>,
-  },
-  {
-    path: "/HomePage",
-    element: <HomePage></HomePage>
-  },
-  {
-    path: "ProductsPage",
-    element: <ProductsPage></ProductsPage>
-  },
-  {
-    path:"/Team",
-    element: <Team></Team>
-  }
-]);
-
-export default router;
+import HomeAfterLogin from "src/components/HomeAfterLogin/HomePageLogin";
+const router = createBrowserRouter(
+    [
+        {
+            path:"/",
+            element:<HomePage></HomePage>
+        },
+        {
+            path:"src\components\HomeAfterLogin\HomePageLogin",
+            element:<HomeAfterLogin></HomeAfterLogin>
+        }
+    ]
+)
