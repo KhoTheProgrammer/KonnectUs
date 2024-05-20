c33f1b152d5a1f7897a21245960c17e56cb6bde8
Rbitah <bsc-35-21@unima.ac.mw>
2024-05-20 19:18:33 +0200

    staging to fix conficts

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,11 @@ import HomePage from "./components/HomePage/HomePage";
 import HomeAfterLogin from "src/components/HomeAfterLogin/HomePageLogin.js"
 const router = createBrowserRouter(
     [
-  {
+        {
             path:"/",
             element:<HomePage></HomePage>
-  },
-  {
+        },
+        {
             path:"src\components\HomeAfterLogin\HomePageLogin",
             element:<HomeAfterLogin></HomeAfterLogin>
         }
