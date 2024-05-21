bed3b4ba50d2aaf579a24065f10d4a9ccdfa0a9a
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-21 22:56:43 +0200

    Changed the width for the div

--- src/components/AccountManagement/AccountPage.js
+++ src/components/AccountManagement/AccountPage.js
@@ -1,13 +1,19 @@
 import Dashboard from "./Dashboard";
 import Update from "./Update";
+import NavBar from "../HomePage/NavBar";
+import Footer from "../HomePage/Footer";
 
 export default function AccountPage() {
   return (
-    <div className="flex gap-4">
-      <Dashboard></Dashboard>
-      <div className="flex h-screen justify-center items-center mx-auto">
-        <Update />
+    <div>
+      <NavBar></NavBar>
+      <div className="flex gap-4">
+        <Dashboard></Dashboard>
+        <div className="w-3/4 flex justify-center mx-auto">
+          <Update />
+        </div>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
