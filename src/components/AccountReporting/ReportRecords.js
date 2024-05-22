ae4d60785abda4e66aedd5883f950682e11075a5
sol1305 <solophinamkandawire@unima.ac.mw>
2024-05-22 23:43:24 +0200

    created a component ReportRecorts and imported a header and a footer to the component

---
+++ src/components/AccountReporting/ReportRecords.js
@@ -0,0 +1,16 @@
+import Footer from "../HomePage/Footer";
+import NavBar from "../HomePage/NavBar";
+
+export default function ReportRecords(){
+    return(
+        <div>
+            <NavBar></NavBar>
+            <div>
+                
+            </div>
+            <Footer></Footer>
+        </div>
+        
+        
+    )
+}
