c3f91f0fb3adc96a1b829f833afb8f8adb473f44
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-05-22 22:46:56 +0200

    added key attribute to list tag in mapButtons function

--- src/components/HomePage/NavBar.js
+++ src/components/HomePage/NavBar.js
@@ -1,53 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import KonnectUslogo from "./Konnect1.png";
 import Search from "../Search and Filtering/Search";
 import { NavLink, useLocation } from "react-router-dom";
-import DropDown from './DropDown';
+import DropDown from "./DropDown";
 
-export default function NavBar({ isSignedIn, setSignedIn, searchTerm, setSearchTerm }) {
+export default function NavBar({
+  isSignedIn,
+  setSignedIn,
+  searchTerm,
+  setSearchTerm,
+}) {
   const [showDropDown, setShowDropDown] = useState(false);
   const location = useLocation();
-
-
 
   const buttons = [
     {
       name: "Home",
-      link: "/HomePage"
+      link: "/HomePage",
     },
     {
       name: "About Us",
-      link: "/AboutUs/AboutUs"
+      link: "/AboutUs/AboutUs",
     },
     {
       name: "Products",
-      link: "/Alt/ProductsPage"
+      link: "/Alt/ProductsPage",
     },
     {
       name: "Team",
-      link: "/Team"
+      link: "/Team",
     },
     {
       name: "Reviews",
-      link: "/RatingAndReviews/ReviewsPage"
+      link: "/RatingAndReviews/ReviewsPage",
     },
     {
       name: "Contact Us",
-      link: "/ContactUs"
-    }
-  ]
+      link: "/ContactUs",
+    },
+  ];
 
   const mapButtons = (button) => (
-    <NavLink to={button.link}>
-      <li className=" font-bold mx-6 text-lg hover:text-green-500 py-1 cursor-pointer">
-        {button.name}
-      </li>
-    </NavLink>)
-
+    <li
+      key={button.name}
+      className=" font-bold mx-6 text-lg hover:text-green-500 py-1 cursor-pointer"
+    >
+      <NavLink to={button.link}>{button.name}</NavLink>
+    </li>
+  );
 
   return (
     <div className="grid grid-cols-[1fr_3fr] p-3 m-0 h-[80px]">
-      <img src={KonnectUslogo} alt="KonnectUs Logo" className="mb-4 h-14 w-[50]" />
+      <img
+        src={KonnectUslogo}
+        alt="KonnectUs Logo"
+        className="mb-4 h-14 w-[50]"
+      />
       <ul className="flex p-3 ">
         {buttons.map(mapButtons)}
         {isSignedIn ? (
@@ -67,16 +75,13 @@ export default function NavBar({ isSignedIn, setSignedIn, searchTerm, setSearchT
             </li>
           </NavLink>
         )}
-          
-          {location.pathname === '/Alt/ProductsPage' && (
+
+        {location.pathname === "/Alt/ProductsPage" && (
           <li className="ml-6">
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </li>
         )}
-
-          
       </ul>
-
     </div>
   );
 }
