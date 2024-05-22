553ddc3d57dc3c3f4b284f4fd4a93491b6803f76
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-22 20:08:49 +0200

    Changed height to full

--- src/components/AccountManagement/SideBar.js
+++ src/components/AccountManagement/SideBar.js
@@ -1,7 +1,7 @@
 export default function SideBar() {
   return (
     <div>
-      <div className=" h-screen bg-green-500 w-52">
+      <div className=" h-full bg-green-500 w-52">
         <div className=" p-4">
           <p className=" text-white font-bold mb-7 text-center text-xl">DASHBOARD</p>
           <p className=" text-white font-bold border-b-2 mb-4">USERS</p>
