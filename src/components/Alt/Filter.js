91dde9e677e48c463e7e332847b939b4a85cd7f2
Rbitah <bsc-35-21@unima.ac.mw>
2024-05-22 16:34:51 +0200

    Fixed filtering options

---
+++ src/components/Alt/Filter.js
@@ -0,0 +1,20 @@
+// src/components/Filter.js
+import React from 'react';
+
+const Filter = ({ filter, setFilter }) => {
+  return (
+    <select
+      value={filter}
+      onChange={(e) => setFilter(e.target.value)}
+      className="p-2 border rounded"
+    >
+      <option value="">Filter by</option>
+      <option value="name">Product Name</option>
+      <option value="price">Price</option>
+      <option value="location">Location</option>
+      <option value="quantity">Quantity</option>
+    </select>
+  );
+};
+
+export default Filter;
