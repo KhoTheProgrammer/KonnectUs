6f2bceca3a75755ccee031e2bec9384df477103f
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-23 00:49:17 +0200

    Added loic to dynamically render the components on the side bar

--- src/components/AccountManagement/Admin.js
+++ src/components/AccountManagement/Admin.js
@@ -1,8 +1,25 @@
-import SideBar from "./SideBar"
-import Users from "./Users"
-export default function Admin(){
-    return <div className=" flex w-full">
-        <SideBar></SideBar>
-        <Users></Users>
+import { useState } from "react";
+import SideBar from "./SideBar";
+import Users from "./Users";
+import Change from "./Change";
+import { useNavigate } from "react-router";
+export default function Admin() {
+  const navigate = useNavigate();
+  const [click, setClick] = useState(1);
+
+  function renders() {
+    if (click === 1) {
+      return <Users></Users>;
+    } else if (click === 2) {
+      return <Change></Change>;
+    } else {
+      navigate("/HomePage");
+    }
+  }
+  return (
+    <div className=" flex w-full">
+      <SideBar click={click} setClick={setClick}></SideBar>
+      {renders()}
     </div>
+  );
 }
