14e6589cd13e8110f3eddf133fd19e80dfac8145
Rbitah <bsc-35-21@unima.ac.mw>
2024-05-22 20:35:02 +0200

    added new photos in slideshow

--- src/components/HomePage/Main.js
+++ src/components/HomePage/Main.js
@@ -8,8 +8,9 @@ function Main(props) {
     "https://oxfordvitality.co.uk/cdn/shop/articles/13929060_ml-e1484929844678.jpg",
     "https://www.greenlife.co.ke/wp-content/uploads/2022/04/Tomatoes-Farm-scaled-1-1536x1024.jpg",
     "https://get.pxhere.com/photo/nature-grass-plant-field-farm-lawn-meadow-prairie-sunlight-leaf-flower-green-crop-agriculture-farmland-in-rural-areas-grassland-habitat-views-ecosystem-natural-environment-grass-family-plant-stem-computer-wallpaper-land-plant-every-day-793337.jpg",
-    "https://www.greenlife.co.ke/wp-content/uploads/2022/04/farming_passion.jpg"
-  ];   
+    "https://www.greenlife.co.ke/wp-content/uploads/2022/04/farming_passion.jpg",
+    "https://as2.ftcdn.net/v2/jpg/04/81/76/79/1000_F_481767956_lPVpcVJt7HseHsUi2ThNIcWVNUs7Q6mm.jpg"
+  ];
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
