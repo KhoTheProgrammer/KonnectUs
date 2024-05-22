742ba8d52a095d72c0bc3e111d45d91b40cf918b
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-05-22 15:55:28 +0200

    added background image to messaging ui

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -4,7 +4,7 @@ import UserMessages from "./UserMessages";
 import MessageBox from "./MessageBox";
 import MessagesNavBar from "./MessageNavBar";
 import SideBar from "./SideBar";
-import Background from './bg-img.jpg';
+import Background from "./bg-img.jpg";
 import { useState } from "react";
 
 export default function Messages() {
@@ -26,44 +26,37 @@ export default function Messages() {
       timestamp: 3,
     },
     {
-      text : "I wanted to show you something",
-      id : "other",
-      timestamp : 4
+      text: "I wanted to show you something",
+      id: "other",
+      timestamp: 4,
     },
     {
-      text : "I found something cool online",
-      id : "other",
-      timestamp : 5
-    }
+      text: "I found something cool online",
+      id: "other",
+      timestamp: 5,
+    },
   ]);
 
-
   return (
-          
-    <div className="flex flex-row" style={{
-      backgroundImage: `url(${Background})`
-    }}>
+    <div
+      className="flex flex-row"
+      style={{
+        backgroundImage: `url(${Background})`,
+      }}
+    >
       <div className="w-0 h-0 md:w-[30%] md:h-full bg-green-300 ">
-      <SideBar/>
+        <SideBar />
       </div>
-     <div className="w-full lg:min-h-full"> 
-    <div className=" flex flex-col">
-      <ul>
-        <li className=" md:w-[70%]">
-        <MessagesNavBar />
-        </li>
-        <div className="h-[110px]">
-        <br></br>
-        </div>
-        <li className="  md:w-[70%]">
+      <div className="w-full lg:min-h-full">
+        <div className=" flex flex-col">
+          <MessagesNavBar />
+          <div className="h-[110px]">
+            <br></br>
+          </div>
           <UserMessages messages={messages} />
-        </li>
-        <li className="">
-          <MessageBox messages = {messages}  updateMessages = {updateMessages}/>
-        </li>
-      </ul>
-    </div>
-    </div>
+          <MessageBox messages={messages} updateMessages={updateMessages} />
+        </div>
+      </div>
     </div>
   );
 }
