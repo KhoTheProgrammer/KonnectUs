9125fcdcdafc0d6b19d3db80d713788f85ff8823
“osmanDaud” <imwakabira@unima.ac.mw>
2024-05-22 17:27:32 +0200

    worked on AboutUs, Login, and SignUp

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
+import NavBar from "../HomePage/NavBar";
+import Footer from "../HomePage/Footer";
 
 class AboutUs extends Component {
   render() {
     return (
       <div>
-        header
+       <NavBar></NavBar>
         <div>
+        <div
+        className="bg-cover bg-center h-[750px] flex items-center justify-center py-3"
+        style={{
+          backgroundImage:
+            "url(https://www.mowernew.com/wp-content/uploads/2023/07/pexels-gilmer-diaz-estela-6345502-1200x800.jpg)",
+        }} >
          
-          <div class=" bg-green-500 w-[60%] h-screen  bg-opacity-50  p-[12px] font-bold mt-[80px] rounded-tr-[80px]">
-            <h1 class=" text-amber-800  text-[18px] ">About Us</h1>
-            <p class=" text-white flex ">
+          <div class=" bg-green-500 w-[60%] h-[550px] mr-[500px]  p-[12px] font-bold mt-[80px] rounded-[80px]">
+            <h1 class=" text-black  text-[22px] flex justify-center items-center"><b>About Us</b></h1>
+            <p class=" text-white text-[18px] ">
               KonnectUs Farmer's Zone is dedicated to revolutionizing
               agricultural communities by connecting farmers with buyers. our
               platform aims to empower farmers, streamline the suply chain, and
@@ -19,7 +27,11 @@ class AboutUs extends Component {
               creating a more sustainable and equitable food system.
             </p>
           </div>
-        </div>
+          </div>
+          
+       
+      </div>
+      <Footer></Footer>
       </div>
     );
   }
