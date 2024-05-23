693a0a85e4a78636195decdff6751524b241df52
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-05-23 18:38:21 +0200

    restructured app.js and wrapped it in a user context provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
+import React from "react";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router}>
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  </RouterProvider>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
