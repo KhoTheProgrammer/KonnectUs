087360d13e18298140576eb5220496dec1b7aaa3
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-26 03:25:39 +0200

    Added logic to send data from user to ggogle forms

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -3,39 +3,18 @@ import Footer from "../HomePage/Footer";
 import NavBar from "../HomePage/NavBar";
 
 const ContactUs = () => {
-  const [formdata, setFormdata] = useState({
-    fname: "",
-    lname: "",
-    email: "",
-    phonenumber: "",
-    message: "",
-  });
+  const handleChange = (e) => {};
 
-  const handleChange = (e) => {
-    setFormdata({
-      ...formdata,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
+    const formEle = document.querySelector("form");
     e.preventDefault();
-    try {
-      const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbwKuPdtFhZIZt2dz3Ie3NiE9-zg16R1Lb4S_ZZjG9sbDXZQsYJzMia0IVNFeEPfxIDb/exec",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formdata),
-        }
-      );
-      const result = await response.json();
-      console.log(result);
-    } catch (error) {
-      console.log(error);
-    }
+    const formdata = new FormData(formEle);
+    fetch(
+      "https://script.google.com/macros/s/AKfycbyFgZQmQ_jzR_9_-yO0v-cbG6KE8Np_RS1Tw5_ovXkKQl5Dliev8KU282gS5p0VRcRhCg/exec", {
+        method: "POST",
+        body: formdata
+      }
+    );
   };
 
   return (
@@ -49,7 +28,7 @@ const ContactUs = () => {
         }}
       >
         <form
-          className="bg-white md:w-1/2 w-3/4 h-3/4 p-6 m-8 rounded-lg"
+          className="bg-white md:w-1/2 w-3/4 h-3/4 p-6 m-8 rounded-lg form"
           onSubmit={handleSubmit}
         >
           <h1 className="text-center md:text-4xl font-bold text-lg mb-4 text-green-500">
@@ -60,10 +39,10 @@ const ContactUs = () => {
           <input
             type="text"
             className="w-full mb-4 rounded-md"
-            name="fname"
+            name="FirstName"
             required
             onChange={handleChange}
-            value={formdata.fname}
+           
           />
           <br />
           <label className="font-bold">Last Name</label>
@@ -71,10 +50,10 @@ const ContactUs = () => {
           <input
             type="text"
             className="w-full mb-4 rounded-md"
-            name="lname"
+            name="LastName"
             required
             onChange={handleChange}
-            value={formdata.lname}
+           
           />
           <br />
           <label className="font-bold">Email</label>
@@ -82,10 +61,10 @@ const ContactUs = () => {
           <input
             type="email"
             className="w-full mb-4 rounded-md"
-            name="email"
+            name="Email"
             required
             onChange={handleChange}
-            value={formdata.email}
+          
           />
           <br />
           <label className="font-bold">Phone Number</label>
@@ -93,20 +72,20 @@ const ContactUs = () => {
           <input
             type="tel"
             className="w-full mb-4 rounded-md"
-            name="phonenumber"
+            name="PhoneNumber"
             required
             onChange={handleChange}
-            value={formdata.phonenumber}
+         
           />
           <br />
           <label className="font-bold">Message</label>
           <br />
           <textarea
             className="w-full mb-8 rounded-md"
-            name="message"
+            name="Message"
             required
             onChange={handleChange}
-            value={formdata.message}
+      
           ></textarea>
           <br />
           <button
