0f0b1c7246298d2289b1b1709efc5903d88a5658
chimwemwekatenje <bsc-26-21@unima.ac.mw>
2024-05-26 03:51:27 -1000

    changed pic

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 import kho from "./IMG_0134.jpg";
 import eric from "./IMG_9799.jpg";
 import daud from "./IMG_3908.jpg";
+import chimwemwe from "./chimmwepic.png";
 
 const Team = () => {
   const [members, setmembers] = useState([
