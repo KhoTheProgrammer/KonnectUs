ec91ae58ac6f23e7687a732d4c68cfe7f21af18f
Rbitah <bsc-35-21@unima.ac.mw>
2024-05-26 12:04:36 +0200

    Search component for small screen(600px below)

---
+++ src/components/Search and Filtering/SmallSearch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SmallSearch = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <input
+      type="text"
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      placeholder="Search.."
+      className="h-8 w-40 mt-0.1  ml-20 top-[-10px]   border-[1px] border-black rounded-xl text-center m-0"
+    />
+  );
+};
+
+export default SmallSearch;
