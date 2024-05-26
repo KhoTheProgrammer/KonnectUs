9ed6c6de216c7d30ae1d3843f0e17e377f8634f3
Rbitah <bsc-35-21@unima.ac.mw>
2024-05-26 15:13:36 +0200

    changed style

--- src/components/Search and Filtering/Search.js
+++ src/components/Search and Filtering/Search.js
@@ -7,7 +7,7 @@ const Search = ({ searchTerm, setSearchTerm }) => {
       value={searchTerm}
       onChange={(e) => setSearchTerm(e.target.value)}
       placeholder="Search products.."
-      className="h-8 mt-0.1 ml-[130px] border-[1px] border-black rounded-xl text-center m-0"
+      className="h-9 mb-6 border-[1px]  text-center w-[210px] max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg px-0"
     />
   );
 };
