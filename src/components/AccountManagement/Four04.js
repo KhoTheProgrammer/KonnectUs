adedf672f0a4fd43fb8e173fe9e243b75985a90e
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-27 10:58:35 +0200

    Added a page not found interface

---
+++ src/components/AccountManagement/Four04.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Four04 = () => {
+  return (
+    <div>
+      <div>
+        <h1>404</h1>
+        <h1>Sorry we couldn't find this page.</h1>
+        <h1>But dont worry , you can find plenty of other things on our homepage</h1>
+        <button>Back to Homepage</button>
+      </div>
+    </div>
+  )
+}
+
+export default Four04
