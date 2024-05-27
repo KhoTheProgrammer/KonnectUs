1f0388f61eca25d03ea2a3f0a33a75aa54b45b94
“osmanDaud” <imwakabira@unima.ac.mw>
2024-05-27 11:57:24 +0200

    changed my photo

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 import kho from "./IMG_0134.jpg"
 import eric from "./IMG_9799.jpg"
 import Nduayhuoo from "./Nduayhuoo.jpg"
-import daud from "./IMG_3908.jpg"
+import daud from "./IMG_3900.jpg"
 
 const Team = () => {
   const [members, setmembers] = useState([
