5261aede54a54642fc5c4286f598c506a913346a
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-27 11:12:14 +0200

    styled the component

--- src/components/AccountManagement/Four04.js
+++ src/components/AccountManagement/Four04.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Four04 = () => {
   return (
-    <div>
-      <div>
-        <h1>404</h1>
-        <h1>Sorry we couldn't find this page.</h1>
-        <h1>But dont worry , you can find plenty of other things on our homepage</h1>
-        <button>Back to Homepage</button>
+    <div className=" bg-white flex justify-center h-screen items-center">
+      <div className="bg-gray-300 p-16 rounded-lg">
+        <div className="">
+          <h1 className="text-5xl mb-4 font-bold">404</h1>
+          <h1 className=" text-3xl">Sorry we couldn't find this page.</h1>
+          <h1 className=" mb-4">
+            But dont worry, you can find plenty of other things on our homepage
+          </h1>
+          <button className=" bg-green-500 p-2 rounded-lg">Back to Homepage</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Four04
+export default Four04;
